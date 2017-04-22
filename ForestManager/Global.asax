c748a335fa5c5120e8713a75d440fc440da070bc
Abdoul Dia <Abdoul Dia>
2017-04-21 21:11:21 -0400

    ForestManager v1

---
+++ ForestManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ForestManager.MvcApplication" Language="C#" %>
---
+++ ForestManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ForestManager.Startup))]
+namespace ForestManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
